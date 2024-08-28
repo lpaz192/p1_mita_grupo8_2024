bc31852e96490eb0a989d597534e738de65e4381
lpaz192 <lpaz@uade.edu.ar>
2024-08-28 17:15:15 -0300

    funcion agregar y leer

--- main.py
+++ main.py
@@ -13,16 +13,16 @@ post_publicacion = [[]]
 # menu 
 menu=0
 while menu!=-1:
-    print("1. para agregar")
-    print("2. para leer ")
-    print("3. para actualizar")
-    print("4. para eliminar ")
-    print("-1. para cancelar")
-    menu=input("Ingrese un nemero: ")
+    print("1. Para agregar")
+    print("2. Para leer ")
+    print("3. Para actualizar")
+    print("4. Para eliminar ")
+    print("-1. Para cancelar")
+    menu=int(input("Ingrese un nemero: "))
 
     if menu==1:
         usuario.append(crud.agregar(len(usuario[0])))
         print(usuario)
     elif menu==2:
-        crud.leer
+        crud.leer(usuario)
 
