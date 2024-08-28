9baa199f85bb98a9a731152c4b4eab244c94e8a7
lpaz192 <lpaz@uade.edu.ar>
2024-08-28 17:16:55 -0300

    .

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ while menu!=-1:
     print("3. Para actualizar")
     print("4. Para eliminar ")
     print("-1. Para cancelar")
-    menu=int(input("Ingrese un nemero: "))
+    menu=int(input("Ingrese un numero: "))
 
     if menu==1:
         usuario.append(crud.agregar(len(usuario[0])))
