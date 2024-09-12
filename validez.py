2cb44e5b24f4a7443170e73c08c6efcbc788694b
mmareco <mmareco@uade.edu.ar>
2024-09-11 21:06:06 -0300

    Validacion de fechas

--- validez.py
+++ validez.py
@@ -1,4 +1,6 @@
 import re
+from datetime import datetime
+
 patron_mail = r"^[a-zA-Z0-9_]{2,20}+@[a-zA-Z]{2,10}+\.[a-zA-Z]{2,10}"
 
 
@@ -6,3 +8,9 @@ patron_mail = r"^[a-zA-Z0-9_]{2,20}+@[a-zA-Z]{2,10}+\.[a-zA-Z]{2,10}"
 def validar_mail(mail):
     return re.match(patron_mail, mail)
 
+def validar_fecha(fecha):
+    try: 
+        datetime.strtime(fecha, '%Y-%m-%d')
+        return True 
+    except ValueError:
+        return False
